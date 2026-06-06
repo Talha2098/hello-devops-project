65b253c49699c1cf282ad32108d12e24f85083bd
Ubuntu <ubuntu@ip-172-31-46-45.ec2.internal>
2026-06-06 05:57:06 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello from Node.js running inside Docker on EC2 🚀");
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.end(`
+        <html>
+            <head>
+                <title>My Site</title>
+            </head>
+            <body>
+                <h1>Hello from Node.js on EC2 🚀</h1>
+                <p>Your server is working correctly!</p>
+            </body>
+        </html>
+    `);
 });
 
 server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server running on port 3000");
 });
